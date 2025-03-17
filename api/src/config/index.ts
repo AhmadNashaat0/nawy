de8c2a890ff9594c03edfb7e5c36c545dca7ac44
Ahmed Nashaat <ahmednashaat.work@gmail.com>
2025-03-17 15:48:10 +0200

    config: (env) create env and env.example file and validate env using zod

---
+++ api/src/config/index.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+const EnvSchema = z.object({
+  PORT: z.coerce.number().default(3000),
+  NODE_ENV: z.enum(['development', 'production']).default('development'),
+  DATABASE_URL: z.string().url(),
+  POOL_SIZE: z.coerce.number().default(10),
+  DB_IDLE_TIMEOUT: z.coerce.number().default(30000),
+});
+
+export const env = EnvSchema.parse(process.env);
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,11 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@/app.module';
+import { env } from '@/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = env.PORT;
+  await app.listen(port);
 }
 bootstrap();
