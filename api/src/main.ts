9f6b9c7b2eb1cb16e979ef2d18960c5356dc9b3f
Ahmed Nashaat <ahmednashaat.work@gmail.com>
2025-03-17 19:36:51 +0200

    config: enable cors , versioning and swagger apis doc

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,10 +2,42 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 import { env } from '@/config';
+import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = env.PORT;
-  await app.listen(port);
+
+  app.enableCors();
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  //swagger
+
+  const openApiConfig = new DocumentBuilder()
+    .setTitle('Nawy API')
+    .setDescription('Nawy API documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, openApiConfig, {
+    deepScanRoutes: true,
+  });
+
+  SwaggerModule.setup('api-docs', app, document, {
+    ui: false,
+  });
+
+  app.use('/api-docs', apiReference({ content: document }));
+
+  await app.listen(port, async () => {
+    const url = await app.getUrl();
+    console.log(`Server is running on ${url}`);
+    console.log(`Api docs is running on ${url}/api-docs`);
+  });
 }
 bootstrap();
